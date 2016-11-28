f0d910d04147948d0b7501be574f5fed6ed4fab8
Phecda <imthe666st@gmail.com>
2016-11-28 14:42:32 +0100

    Chatbox and debugbox write

--- Maoubot-GUI/Maoubot.Designer.cs
+++ Maoubot-GUI/Maoubot.Designer.cs
@@ -34,17 +34,28 @@
 			this.Debugbox = new System.Windows.Forms.TextBox();
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.buttonConfigLoad = new System.Windows.Forms.Button();
+			this.buttonConfigSave = new System.Windows.Forms.Button();
+			this.textBoxChannel = new System.Windows.Forms.TextBox();
+			this.textBoxOAuth = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.buttonConnect = new System.Windows.Forms.Button();
+			this.textBoxNickname = new System.Windows.Forms.TextBox();
 			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.tableLayoutPanel2.SuspendLayout();
 			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tableLayoutPanel1
 			// 
 			this.tableLayoutPanel1.ColumnCount = 2;
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 62.78281F));
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.21719F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 350F));
 			this.tableLayoutPanel1.Controls.Add(this.Chatbox, 0, 0);
 			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -52,7 +63,6 @@
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 1;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
 			this.tableLayoutPanel1.Size = new System.Drawing.Size(884, 612);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
@@ -62,8 +72,9 @@
 			this.Chatbox.Location = new System.Drawing.Point(3, 3);
 			this.Chatbox.Multiline = true;
 			this.Chatbox.Name = "Chatbox";
+			this.Chatbox.ReadOnly = true;
 			this.Chatbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.Chatbox.Size = new System.Drawing.Size(549, 606);
+			this.Chatbox.Size = new System.Drawing.Size(528, 606);
 			this.Chatbox.TabIndex = 0;
 			// 
 			// tableLayoutPanel2
@@ -74,22 +85,23 @@
 			this.tableLayoutPanel2.Controls.Add(this.Debugbox, 0, 1);
 			this.tableLayoutPanel2.Controls.Add(this.tabControl1, 0, 0);
 			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel2.Location = new System.Drawing.Point(558, 3);
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(537, 3);
 			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
 			this.tableLayoutPanel2.RowCount = 2;
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 39.43895F));
-			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60.56105F));
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(323, 606);
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 29.37294F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70.62706F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(344, 606);
 			this.tableLayoutPanel2.TabIndex = 1;
 			// 
 			// Debugbox
 			// 
 			this.Debugbox.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.Debugbox.Location = new System.Drawing.Point(3, 242);
+			this.Debugbox.Location = new System.Drawing.Point(3, 181);
 			this.Debugbox.Multiline = true;
 			this.Debugbox.Name = "Debugbox";
+			this.Debugbox.ReadOnly = true;
 			this.Debugbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-			this.Debugbox.Size = new System.Drawing.Size(317, 361);
+			this.Debugbox.Size = new System.Drawing.Size(338, 422);
 			this.Debugbox.TabIndex = 0;
 			// 
 			// tabControl1
@@ -100,28 +112,126 @@
 			this.tabControl1.Location = new System.Drawing.Point(3, 3);
 			this.tabControl1.Name = "tabControl1";
 			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(317, 233);
+			this.tabControl1.Size = new System.Drawing.Size(338, 172);
 			this.tabControl1.TabIndex = 1;
 			// 
 			// tabPage1
 			// 
+			this.tabPage1.Controls.Add(this.label3);
+			this.tabPage1.Controls.Add(this.label2);
+			this.tabPage1.Controls.Add(this.label1);
+			this.tabPage1.Controls.Add(this.buttonConfigLoad);
+			this.tabPage1.Controls.Add(this.buttonConfigSave);
+			this.tabPage1.Controls.Add(this.textBoxChannel);
+			this.tabPage1.Controls.Add(this.textBoxOAuth);
+			this.tabPage1.Controls.Add(this.button2);
+			this.tabPage1.Controls.Add(this.buttonConnect);
+			this.tabPage1.Controls.Add(this.textBoxNickname);
 			this.tabPage1.Location = new System.Drawing.Point(4, 22);
 			this.tabPage1.Name = "tabPage1";
 			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage1.Size = new System.Drawing.Size(309, 207);
+			this.tabPage1.Size = new System.Drawing.Size(330, 146);
 			this.tabPage1.TabIndex = 0;
 			this.tabPage1.Text = "Login/Connect";
 			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// buttonConfigLoad
+			// 
+			this.buttonConfigLoad.Location = new System.Drawing.Point(169, 84);
+			this.buttonConfigLoad.Name = "buttonConfigLoad";
+			this.buttonConfigLoad.Size = new System.Drawing.Size(155, 25);
+			this.buttonConfigLoad.TabIndex = 6;
+			this.buttonConfigLoad.Text = "Load Config";
+			this.buttonConfigLoad.UseVisualStyleBackColor = true;
+			// 
+			// buttonConfigSave
+			// 
+			this.buttonConfigSave.Location = new System.Drawing.Point(6, 84);
+			this.buttonConfigSave.Name = "buttonConfigSave";
+			this.buttonConfigSave.Size = new System.Drawing.Size(155, 25);
+			this.buttonConfigSave.TabIndex = 5;
+			this.buttonConfigSave.Text = "Save Config";
+			this.buttonConfigSave.UseVisualStyleBackColor = true;
+			// 
+			// textBoxChannel
+			// 
+			this.textBoxChannel.Location = new System.Drawing.Point(65, 58);
+			this.textBoxChannel.Name = "textBoxChannel";
+			this.textBoxChannel.Size = new System.Drawing.Size(259, 20);
+			this.textBoxChannel.TabIndex = 4;
+			this.textBoxChannel.Text = "CHANNEL";
+			// 
+			// textBoxOAuth
+			// 
+			this.textBoxOAuth.Location = new System.Drawing.Point(65, 32);
+			this.textBoxOAuth.Name = "textBoxOAuth";
+			this.textBoxOAuth.Size = new System.Drawing.Size(259, 20);
+			this.textBoxOAuth.TabIndex = 3;
+			this.textBoxOAuth.Text = "OAUTH_KEY";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(169, 115);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(155, 25);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Disconnect/Quit";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// buttonConnect
+			// 
+			this.buttonConnect.Location = new System.Drawing.Point(6, 115);
+			this.buttonConnect.Name = "buttonConnect";
+			this.buttonConnect.Size = new System.Drawing.Size(155, 25);
+			this.buttonConnect.TabIndex = 1;
+			this.buttonConnect.Text = "Connect";
+			this.buttonConnect.UseVisualStyleBackColor = true;
+			this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+			// 
+			// textBoxNickname
+			// 
+			this.textBoxNickname.Location = new System.Drawing.Point(65, 6);
+			this.textBoxNickname.Name = "textBoxNickname";
+			this.textBoxNickname.Size = new System.Drawing.Size(259, 20);
+			this.textBoxNickname.TabIndex = 0;
+			this.textBoxNickname.Text = "NICKNAME";
 			// 
 			// tabPage2
 			// 
 			this.tabPage2.Location = new System.Drawing.Point(4, 22);
 			this.tabPage2.Name = "tabPage2";
 			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(192, 74);
+			this.tabPage2.Size = new System.Drawing.Size(330, 146);
 			this.tabPage2.TabIndex = 1;
 			this.tabPage2.Text = "tabPage2";
 			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(4, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 13);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "Nickname";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 35);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(56, 13);
+			this.label2.TabIndex = 8;
+			this.label2.Text = "oAuth Key";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(6, 61);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(46, 13);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "Channel";
 			// 
 			// Maoubot
 			// 
@@ -130,12 +240,14 @@
 			this.ClientSize = new System.Drawing.Size(884, 612);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "Maoubot";
-			this.Text = "Form1";
+			this.Text = "Maoubot-GUI";
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.tableLayoutPanel1.PerformLayout();
 			this.tableLayoutPanel2.ResumeLayout(false);
 			this.tableLayoutPanel2.PerformLayout();
 			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -149,6 +261,16 @@
 		private System.Windows.Forms.TabControl tabControl1;
 		private System.Windows.Forms.TabPage tabPage1;
 		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TextBox textBoxChannel;
+		private System.Windows.Forms.TextBox textBoxOAuth;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button buttonConnect;
+		private System.Windows.Forms.TextBox textBoxNickname;
+		private System.Windows.Forms.Button buttonConfigLoad;
+		private System.Windows.Forms.Button buttonConfigSave;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
 	}
 }
 
--- Maoubot-GUI/Maoubot.cs
+++ Maoubot-GUI/Maoubot.cs
@@ -9,41 +9,211 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TwitchSharp;
+using TwitchSharp.EventArguments;
 using TwitchSharp.Utilities;
 
 namespace Maoubot_GUI
 {
 	public partial class Maoubot : Form
 	{
-		private readonly String ConfigPath = @"Config\";
+		private static readonly String ConfigPath = @"Config\";
+		private static readonly String TwitchConfigPath = ConfigPath + @"twitch.xml";
+		private static readonly String QuotesConfigPath = ConfigPath + @"quotes.xml";
 
 		private ConfigFile Cf;
 		private QuoteFile Qf;
+
+		private TwitchChatBot Tcb;
 
 		[DllImport("kernel32.dll", SetLastError = true)]
 		[return: MarshalAs(UnmanagedType.Bool)]
 		static extern bool AllocConsole();
 
+
+		/// <summary>
+		/// Maoubot-GUI
+		/// A simple chatbot for use on Twitch.tv ( irc.twitch.tv:6667 )
+		/// </summary>
 		public Maoubot()
 		{
 			InitializeComponent();
-			AllocConsole();
+			AllocConsole(); // Console for debugging usage.
 
-			this.Cf = ConfigFile.LoadFromXml(ConfigPath+"twitch.xml");
-			if (Cf == null)
-			{
-				this.Cf = new ConfigFile();
-				ConfigFile.SaveToXml(ConfigPath + "twitch.xml", Cf);
-			}
+			this.Load += LoadForm;
 
-			this.Qf = QuoteFile.LoadFromXml(ConfigPath + "quotes.xml");
-			if (Qf == null)
+			
+		}
+
+		/// <summary>
+		/// Load Event Method
+		/// Loads important assets ( Twitch.xml, etc )
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void LoadForm(object sender, EventArgs e)
+		{
+			LoadTwitchConfig();
+			LoadQuoteConfig();
+			CreateTwitchChatBot();
+		}
+
+		/// <summary>
+		/// Append Message to the Chatbot
+		/// </summary>
+		/// <param name="Message"></param>
+		private void LogWrite(String Message)
+		{
+			if (Chatbox.InvokeRequired)
 			{
-				this.Qf = new QuoteFile();
-				QuoteFile.SaveToXml(ConfigPath + "quotes.xml", Qf);
+				Chatbox.Invoke(new Action(() => { Chatbox.AppendText(Message); }));
 			}
-			Console.WriteLine(Qf.Quotes.Length);
-			foreach (var s in Qf.Quotes) Console.WriteLine(s);
+			else
+			{
+				Chatbox.AppendText(Message);
+			}
+		}
+		
+		/// <summary>
+		/// Append Message to the Chatbot and a newline.
+		/// </summary>
+		/// <param name="Message"></param>
+		private void LogWriteLine(String Message)
+		{
+			LogWrite(String.Format("{0}\n", Message));
+		}
+		
+		/// <summary>
+		/// Append Message to the Debugbox
+		/// </summary>
+		/// <param name="Message"></param>
+		private void LogDebugWrite(String Message)
+		{
+			if (Debugbox.InvokeRequired)
+			{
+				Debugbox.Invoke(new Action(() => { Chatbox.AppendText(Message); }));
+			}
+			else
+			{
+				Debugbox.AppendText(Message);
+			}
+		}
+		
+		/// <summary>
+		/// Append Message to the Debugbox and a newline
+		/// </summary>
+		/// <param name="Message"></param>
+		private void LogDebugWriteLine(String Message)
+		{
+			LogDebugWrite(String.Format("{0}\n", Message));
+		}
+
+
+
+		// Config Methods
+
+		/// <summary>
+		/// Saves the TwitchConfig to twitch.xml **XXX_TODO_XXX**
+		/// </summary>
+		/// <param name="ReadFromForm">Shall the config be updated from the form?</param>
+		private void SaveTwitchConfig(bool ReadFromForm = true)
+		{
+			if (this.Cf == null) ConfigFile.SaveToXml(TwitchConfigPath, new ConfigFile());
+			else
+			{
+				if (ReadFromForm)
+				{
+					this.Cf.Nick = textBoxNickname.Text;
+					this.Cf.oAuth = textBoxOAuth.Text;
+					this.Cf.Channel = textBoxChannel.Text;
+				}
+
+				ConfigFile.SaveToXml(TwitchConfigPath, this.Cf);
+			}
+		}
+
+		/// <summary>
+		/// Load your TwitchConfig
+		/// Creates an empty TwitchConfig if none could be found.
+		/// </summary>
+		private void LoadTwitchConfig()
+		{
+			this.Cf = ConfigFile.LoadFromXml(TwitchConfigPath);
+			if (this.Cf == null)
+			{
+				// Save an empty config file and load it
+				// Return to prevent recursion
+				SaveTwitchConfig();
+				LoadTwitchConfig();
+				return;
+			}
+			
+			// Load config to form
+			textBoxNickname.Text = Cf.Nick;
+			textBoxOAuth.Text = Cf.oAuth;
+			textBoxChannel.Text = Cf.Channel;
+		}
+
+		// Quote Methods
+
+		private void SaveQuoteConfig()
+		{
+			if (this.Qf == null) QuoteFile.SaveToXml(QuotesConfigPath, new QuoteFile());
+			else
+			{
+				QuoteFile.SaveToXml(ConfigPath + QuotesConfigPath, Qf);
+				
+			}
+		}
+
+		private void LoadQuoteConfig()
+		{
+			this.Qf = QuoteFile.LoadFromXml(QuotesConfigPath);
+			if (this.Qf == null)
+			{
+				SaveQuoteConfig();
+				LoadQuoteConfig();
+			}
+		}
+
+		// TwitchChatBot Methods
+
+		/// <summary>
+		/// Creates the TCB object and adds events
+		/// </summary>
+		private void CreateTwitchChatBot()
+		{
+			this.Tcb = new TwitchChatBot();
+			this.Tcb.LoginCompleted += (s, e) =>
+			{
+				Tcb.JoinChannel(Cf.Channel);
+			};
+
+			this.Tcb.MessageReceived += Tcb_MessageReceived;
+			this.Tcb.CommandExecute += Tcb_CommandExecute;
+		}
+
+		// Execute a command
+		private void Tcb_CommandExecute(object sender, CommandExecuteEventArgs e)
+		{
+			//throw new NotImplementedException();
+		}
+
+		private void Tcb_MessageReceived(object sender, MessageReceivedEventArgs e)
+		{
+			if (e.MessageType == MessageType.Ping)
+			{
+				Tcb.SendIrcMessage("PONG {0}", Tcb.HOST);
+			}
+			else if (e.MessageType == MessageType.Chat || e.MessageType == MessageType.Whisper)
+			{
+
+			}
+		}
+
+		private void buttonConnect_Click(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
