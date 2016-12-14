e10ba832d7f6436bf4b30c81340e45c62be7d44b
imthe666st <imthe666st@gmail.com>
2016-12-14 23:45:46 +0100

    ???

---
+++ Maoubot-GUI/Component/TextCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchSharp.EventArguments;
+using System.Runtime.Serialization;
+
+namespace Maoubot_GUI.Component
+{
+	[DataContract(IsReference=true)]
+	public class TextCommand
+	{
+		[DataMember]
+		public String Command { get; set; }
+		[DataMember]
+		public String Output { get; set; }
+
+		public TextCommand(String Command, String Output)
+		{
+			this.Command = Command;
+			this.Output = Output;
+		}
+
+		public String Format(CommandExecuteEventArgs e)
+		{
+			String o = Output;
+			o = o.Replace("%nick%", e.Nick);
+			o = o.Replace("%time%", DateTime.Now.ToString());
+
+			return o;
+		}
+
+	}
+}
---
+++ Maoubot-GUI/Xml/BotConfig.cs
@@ -0,0 +1,76 @@
+﻿using Maoubot_GUI.Component;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TwitchSharp.Utilities;
+
+namespace Maoubot_GUI.Xml
+{
+	[DataContract(IsReference = true)]
+	public class BotConfig : XmlManager
+	{
+		[DataMember]
+		public String CommandPrefix { get; set; }
+		[DataMember]
+		public TextCommand[] TextCommands { get; set; }
+
+		public BotConfig()
+			: base()
+		{
+			Init();
+		}
+
+		protected override void Init()
+		{
+			if (String.IsNullOrEmpty(CommandPrefix)) CommandPrefix = "!";
+
+			if (TextCommands == null)
+			{
+				TextCommands = new TextCommand[]
+				{
+					new TextCommand("example", "this is an example %nick%!")
+				};
+			}
+		}
+
+		public new static BotConfig LoadFromXml(string FilePath)
+		{
+
+			try
+			{
+				using (FileStream stream = File.OpenRead(FilePath))
+				{
+					DataContractSerializer xmlSerializer = new DataContractSerializer(MethodBase.GetCurrentMethod().DeclaringType);
+					var asd = xmlSerializer.ReadObject(stream);
+					return (BotConfig)asd;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Unable to load file\n\t{0}", FilePath);
+				//throw ex;
+				return null;
+			}
+		}
+
+		public void AddCommand(String Command, String Output)
+		{
+			TextCommand c = new TextCommand(Command, Output);
+
+			AddCommand(c);
+		}
+
+		public void AddCommand(TextCommand c)
+		{
+			List<TextCommand> a = TextCommands.ToList();
+			a.Add(c);
+			TextCommands = a.ToArray();
+		}
+	}
+}
